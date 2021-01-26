a2f80f5b7db179d5ede28f9c18b30d42c7a1cf00
Jim Gale <jimgaleforce@gmail.com>
2021-01-26 13:44:20 -0800

    added min/max for dates

--- Helpers/TimeHelpers.cs
+++ Helpers/TimeHelpers.cs
@@ -5,7 +5,7 @@ namespace GaleForceCore.Helpers
     public static class TimeHelpers
     {
         /// <summary>
-        /// Unixes the time stamp to date time.
+        /// Return a DateTime for a Unix time stamp.
         /// </summary>
         /// <param name="unixTimeStamp">The unix time stamp.</param>
         /// <returns>DateTime.</returns>
@@ -15,6 +15,58 @@ namespace GaleForceCore.Helpers
             System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
             dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
             return dtDateTime;
+        }
+
+        /// <summary>
+        /// Returns the Max date between two dates. If both null, which shouldn't happen, returns MinValue.
+        /// </summary>
+        /// <param name="d1">The first date.</param>
+        /// <param name="d2">The second date.</param>
+        /// <returns>DateTime.</returns>
+        public static DateTime Max(DateTime d1, DateTime d2)
+        {
+            if(d1 == null && d2 == null)
+            {
+                return DateTime.MinValue;
+            }
+            else if(d1 == null)
+            {
+                return d2;
+            }
+            else if(d2 == null)
+            {
+                return d1;
+            }
+            else
+            {
+                return d1.CompareTo(d2) < 0 ? d2 : d1;
+            }
+        }
+
+        /// <summary>
+        /// Returns the Min date between two dates. If both null, which shouldn't happen, returns MaxValue.
+        /// </summary>
+        /// <param name="d1">The first date.</param>
+        /// <param name="d2">The second date.</param>
+        /// <returns>DateTime.</returns>
+        public static DateTime Min(DateTime d1, DateTime d2)
+        {
+            if(d1 == null && d2 == null)
+            {
+                return DateTime.MaxValue;
+            }
+            else if(d1 == null)
+            {
+                return d2;
+            }
+            else if(d2 == null)
+            {
+                return d1;
+            }
+            else
+            {
+                return d1.CompareTo(d2) < 0 ? d1 : d2;
+            }
         }
     }
 }
